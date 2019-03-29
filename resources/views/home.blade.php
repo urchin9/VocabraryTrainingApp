b485358323e9c6a72db4a7aec6458bd1271d13d4
Azusa Okumura <47806119+urchin9@users.noreply.github.com>
2019-03-30 00:56:19 +0900

    make it better

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-sm-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <div class="card-header">Messanger</div>
+                    <chat-app :user="{{ auth()->user() }}"></chat-app>
+                <div class="card-body home-body" id="app">
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ Route::group(['middleware' => 'auth.very_basic'], function() {
   Route::get('/', function () {
     return view('welcome');
   });
+  Auth::routes();
+
+  Route::get('/home', 'HomeController@index')->name('home');
+  Route::get('/contacts', 'ContactsController@get');
+  Route::get('/conversation/{id}', 'ContactsController@getMessagesFor');
+  Route::post('/conversation/send', 'ContactsController@send');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
